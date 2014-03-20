116f3b11ac3a399cab99a37d44a7490fb7ef0e60
Simon Woolf <simon@simonwoolf.net>
2014-03-20 14:43:31 +0000

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140320142108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cases", force: true do |t|
+    t.text     "title"
+    t.text     "summary"
+    t.text     "facts"
+    t.text     "held"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
