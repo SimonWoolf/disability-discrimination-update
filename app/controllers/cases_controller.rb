293de094899585d826684fb99485ee50f3bd3623
Simon Woolf <simon@simonwoolf.net>
2014-03-20 17:00:29 +0000

    Adding, listing & retreiving a case

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,18 +1,29 @@
 class CasesController < ApplicationController
+  CASE_FIELDS = [:title, :summary, :facts, :held, :comment]
 
   def index
+    @cases = Case.all
   end
 
   def create
+    @case = Case.new(params[:case].permit(CASE_FIELDS))
+    if @case.save
+      redirect_to action: :index
+    else
+      flash.now[:error] = 'Error: failed to save case'
+      render :new
+    end
   end
 
   def new
+    @case = Case.new
   end
 
   def edit
   end
 
   def show
+    @case = Case.find(params[:id])
   end
 
   def update
---
+++ spec/features/cases_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'cases' do
+  specify 'adding a case' do
+    visit '/cases/new'
+    fill_in 'case_title', with: 'test title'
+    fill_in 'case_summary', with: 'test summary'
+    fill_in 'case_facts', with: 'test facts'
+    fill_in 'case_held', with: 'test held'
+    fill_in 'case_comment', with: 'test comment'
+    click_button 'Add case'
+
+    visit '/cases'
+    click_link 'test title'
+    expect(page).to have_content 'test title'
+    expect(page).to have_content 'test summary'
+    expect(page).to have_content 'test facts'
+    expect(page).to have_content 'test held'
+    expect(page).to have_content 'test comment'
+  end
+end
