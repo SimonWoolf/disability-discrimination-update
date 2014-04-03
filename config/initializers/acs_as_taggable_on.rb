9013b8f1539b65b9c76410a4c725595c56ae2feb
Simon Woolf <simon@simonwoolf.net>
2014-04-03 16:37:55 +0100

    dash-seperated taglist

---
+++ config/initializers/acs_as_taggable_on.rb
@@ -0,0 +1,3 @@
+# comma, hyphen, n-dash, m-dash respectively
+# NB spaces around dashes as might have a dash within a tag
+ActsAsTaggableOn.delimiter = [",", " - ", " – ", " — "] 
--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Case do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#keywords' do
+    specify 'comma-seperated keywordlist' do
+      testcase = Case.new(title: "testtitle", summary: "testsummary", facts: "testfacts", held: "testheld", comment: "testcomment")
+      testcase.keyword_list = "Reasonable Adjustments, Burden of Proof, Proof of Adjustment" 
+      testcase.save
+      expect(testcase.keywords.map(&:name)).to eq ["Reasonable Adjustments", "Burden of Proof", "Proof of Adjustment"]
+    end
+
+    specify 'spaced-dash-seperated keywordlist, with dashes inside a keyword' do
+      testcase = Case.new(title: "testtitle", summary: "testsummary", facts: "testfacts", held: "testheld", comment: "testcomment")
+      testcase.keyword_list = "a dash - an n-dash – an m-dash — end" 
+      testcase.save
+      expect(testcase.keywords.map(&:name)).to eq ["a dash", "an n-dash", "an m-dash", "end"]
+    end
+  end
 end
