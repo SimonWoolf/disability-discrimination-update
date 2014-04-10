792dbeffda20e682b5b605510bcace5d0a2a359a
Simon Woolf <simon@simonwoolf.net>
2014-04-10 18:18:42 +0100

    fix keepalive - dont rely on $HOSTNAME

--- config/initializers/keepalive.rb
+++ config/initializers/keepalive.rb
@@ -5,6 +5,6 @@ scheduler = Rufus::Scheduler.new
 
 if Rails.env.production?
   scheduler.every '50m' do
-     Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
+     Net::HTTP.get_response(URI.parse("http://disability-discrimination.herokuapp.com/"))
   end
 end
