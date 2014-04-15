b3052a2e3d79560c2d69a279ea6e5bb7e430590a
Simon Woolf <simon@simonwoolf.net>
2014-04-15 16:50:13 +0100

    capybara poltergeist test for filtering, using database_cleaner to clear up

---
+++ spec/features/filtering_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'filtering', js: true do
+  it 'should filter by name in js on Cases By Name' do
+    Case.create!(title: "goodtitle")
+    Case.create!(title: "badtitle")
+    visit '/cases'
+    fill_in 'namefilter', with: 'good'
+    expect(page).to have_content(:visible, 'goodtitle')
+    expect(page).not_to have_content(:visible, 'badtitle')
+  end
+end
