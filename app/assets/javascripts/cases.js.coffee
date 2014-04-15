030ac1bb1b82f42f245e766fecd60033b85db722
Simon Woolf <simon@simonwoolf.net>
2014-04-15 18:28:53 +0100

    Filter by keyword feature

--- app/assets/javascripts/cases.js.coffee
+++ app/assets/javascripts/cases.js.coffee
@@ -15,3 +15,14 @@ $(document).ready ->
                           .toLowerCase()
                           .indexOf(filter) < 0
     .hide()
+
+  $('#kwfilter').keyup ->
+    $('.keywordrow').show()
+    filter = $('#kwfilter').val()
+                           .trim()
+                           .toLowerCase()
+    $('.keywordrow').filter ->
+      $('.keyword', this).html()
+                         .toLowerCase()
+                         .indexOf(filter) < 0
+    .hide()
--- spec/features/filtering_spec.rb
+++ spec/features/filtering_spec.rb
@@ -5,8 +5,19 @@ describe 'filtering', js: true do
     Case.create!(title: "goodtitle")
     Case.create!(title: "badtitle")
     visit '/cases'
-    fill_in 'namefilter', with: 'good '
+    fill_in 'namefilter', with: ' good '
     expect(page).to have_content(:visible, 'goodtitle')
     expect(page).not_to have_content(:visible, 'badtitle')
+  end
+  
+  it 'should filter by keyword in js on Cases By Keyword' do
+    Case.create!(title: "goodtitle", keyword_list: "happykeyword")
+    Case.create!(title: "badtitle", keyword_list: "sadkeyword")
+    visit '/cases/by-keyword'
+    fill_in 'kwfilter', with: ' happy '
+    expect(page).to have_content(:visible, 'goodtitle')
+    expect(page).to have_content(:visible, 'happykeyword')
+    expect(page).not_to have_content(:visible, 'badtitle')
+    expect(page).not_to have_content(:visible, 'badkeyword')
   end
 end
