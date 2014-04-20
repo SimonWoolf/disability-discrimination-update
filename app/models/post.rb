614f5a22bdf0c87811f2453379fd03dccee853b5
Simon Woolf <simon@simonwoolf.net>
2014-04-20 18:23:30 +0100

    use markdown to render posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   validates_presence_of :body, :title
+
+  def body_html
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    markdown.render(body).html_safe
+  end
 end
