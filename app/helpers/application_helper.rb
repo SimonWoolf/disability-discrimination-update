2fbf54cabb810deec5a4486d0b0785405d1ae43d
Simon Woolf <simon.woolf@econsultancy.com>
2014-09-10 19:48:09 +0100

    Render cases using markdown too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def render_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    markdown.render(text.to_s).html_safe
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,3 @@
 class Post < ActiveRecord::Base
   validates_presence_of :body, :title
-
-  def body_html
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
-    markdown.render(body).html_safe
-  end
 end
